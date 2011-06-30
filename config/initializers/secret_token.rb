9a7a10a475fd7c647bd5c1bd923d8749be6132bc
fakestarbaby <fakestarbaby@gmail.com>
2011-06-30 22:22:26 +0900

    初回コミット。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleTypus::Application.config.secret_token = 'e5e8268c261eee0640687e53a02584a771e0ddad9d10aa3e23bf88a6f674056c46e00196d92fb43b6a0e413130f28153a48072e7d7bcda22f83209a3d2e20028'
