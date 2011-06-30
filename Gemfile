586a5828d34dae08d3df5ff9f42ddadf4a7f88bc
fakestarbaby <fakestarbaby@gmail.com>
2011-06-30 23:13:10 +0900

    typusプラグインをインストール。

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ gem 'rails', '3.0.9'
 
 gem 'paperclip'
 gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'typus'
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,3 +1,4 @@
 class Asset < ActiveRecord::Base
   belongs_to :user
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
